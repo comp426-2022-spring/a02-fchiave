1ad66f82bcaf87b9050946aaeb73465b8a7c72fa
Felipe Chiavegatto <fchiave@ad.unc.edu>
2022-02-03 17:26:35 -0500

    countFlips functional

--- flip.js
+++ flip.js
@@ -1,4 +1,6 @@
 // Import the coinFlip function from your coin.mjs file
 import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 // Call the coinFlip function and put the return into STDOUT
-console.log(coinFlips(4));
+let array = coinFlips(100);
+console.log(array);
+console.log(countFlips(array));
