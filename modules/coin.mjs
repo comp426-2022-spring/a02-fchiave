eef40aded020154bd8c7b681759fb649a1491902
Felipe Chiavegatto <fchiave@ad.unc.edu>
2022-02-03 17:00:26 -0500

    export input work with npm run flip

--- modules/coin.mjs
+++ modules/coin.mjs
@@ -15,7 +15,7 @@
  */
 
 function coinFlip() {
-
+  return Math.random(0, 2) == 0 ? "heads" : "tails"
 }
 
 /** Multiple coin flips
@@ -78,3 +78,4 @@ function flipACoin(call) {
  * 
  * Export all of your named functions
 */
+export {coinFlip, coinFlips, countFlips, flipACoin};
