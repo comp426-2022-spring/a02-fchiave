68b262b90d1282bc195700ea8033a02b7e93d2d9
Felipe Chiavegatto <fchiave@ad.unc.edu>
2022-02-17 01:39:39 -0500

    guessflip error message

--- guess-flip.js
+++ guess-flip.js
@@ -3,15 +3,13 @@ import minimist from 'minimist';
 // Import the coinFlips and countFlips function from your coin.mjs file
 import {flipACoin} from './modules/coin.mjs';
 // Call the coinFlip function and put the return into STDOUT
-console.log("In flips.js")
-console.log(process.argv)
 
 // get argument from command line
 const args = minimist(process.argv.slice(2))
 
 //if no call arg, exit with error
 if (args.call === undefined) {
-    console.log("Error: No input")
+    console.log("Error: no input")
     process.exit(0);
 } 
 
