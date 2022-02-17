44d544df9f14634223294a17641a65a391634c24
Felipe Chiavegatto <fchiave@ad.unc.edu>
2022-02-17 01:37:28 -0500

    autograde test?

--- guess-flip.js
+++ guess-flip.js
@@ -3,6 +3,8 @@ import minimist from 'minimist';
 // Import the coinFlips and countFlips function from your coin.mjs file
 import {flipACoin} from './modules/coin.mjs';
 // Call the coinFlip function and put the return into STDOUT
+console.log("In flips.js")
+console.log(process.argv)
 
 // get argument from command line
 const args = minimist(process.argv.slice(2))
